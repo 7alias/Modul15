90bc1c27ef7c1634667f79c8df2d204fad64e810
Ilya Maximov <xc.navi@gmail.com>
2022-07-08 11:32:05 +0200

    ok

--- src/DirectoriesWork.java
+++ src/DirectoriesWork.java
@@ -4,25 +4,23 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class DirectoriesWork {
     public static void main(String[] args) {
 
         try {
 
-            Files.createDirectories(Paths.get("animals"));
+            Files.createDirectories(Paths.get("files"));
         } catch (IOException e) {
             // Handle file I/O exception
             System.out.println(e.getMessage());
         }
         String currentLine = null;
-        Path path2 = Paths.get("animals/gorilla.txt");
+        Path path = Paths.get("files/gorilla.txt");
 
         List<String> data = new ArrayList<>();
-        try (BufferedWriter writer = Files.newBufferedWriter(path2, Charset.defaultCharset())) {
+        try (BufferedWriter writer = Files.newBufferedWriter(path, Charset.defaultCharset())) {
             String a = "";
             for (int j = 1; j < 10; j++) {
                 a += j;
@@ -37,12 +35,12 @@ public class DirectoriesWork {
 
 
         try {
-            System.out.println(Files.isSameFile(Paths.get("animals/gorilla"), Paths.get("animals/gorilla")));
+            System.out.println(Files.isSameFile(Paths.get("files/gorilla"), Paths.get("files/gorilla")));
 
         } catch (IOException e) {
             // Handle file I/O exception
         }
-        Path path = Paths.get("animals/gorilla.txt");
+
 
         try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.US_ASCII)) { // Выбираем кодировку файла
             // читаем со стрима
@@ -56,33 +54,30 @@ public class DirectoriesWork {
             // Handle file I/O exception
         }
         ReverseText rt = new ReverseText();
-        rt.Reverse("animals/gorilla.txt", "animals/allirog.txt");
+        rt.Reverse("files/gorilla.txt", "files/allirog.txt");
     }
 }
- class ReverseText{
-     void Reverse( String source, String target)
-    {
-        try{
 
-
-            File sourceFile=new File(source);
-
-            Scanner content=new Scanner(sourceFile);
-            PrintWriter pwriter =new PrintWriter(target);
-
-            while(content.hasNextLine())
+class ReverseText {
+    void Reverse(String source, String target) {
+        try {
+            File sourceFile = new File(source);
+            Scanner content = new Scanner(sourceFile);
+            PrintWriter pwriter = new PrintWriter(target);
+            LinkedList<String> linkedList = new LinkedList<>();
+            while (content.hasNextLine())   // сохранить все строки в прямом порядке в linkedList
             {
-                String s=content.nextLine();
+                String s = content.nextLine();
                 StringBuffer buffer = new StringBuffer(s);
-                buffer=buffer.reverse();
-                String rs=buffer.toString();
-                pwriter.println(rs);
+                linkedList.add(String.valueOf(buffer));
+            }
+            Iterator iter = linkedList.descendingIterator();   // объявить итератор в обратном порядке от linkedList
+            while (iter.hasNext()) {               // вывести в обратном порядке
+                pwriter.println(iter.next().toString());
             }
             content.close();
             pwriter.close();
-        }
-
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Something went wrong");
         }
 
